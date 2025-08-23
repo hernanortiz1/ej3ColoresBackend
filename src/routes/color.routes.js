97e02bcf79215a20aa8c647d1f51822d1b8208b4
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-08-23 13:50:25 -0300

    agregue funciones a server y middlewares

---
+++ src/routes/color.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { test } from "../controllers/color.cotrollers.js";
+
+const router = Router()
+
+router.route("/test").get(test)
+
+
+export default router;
---
+++ src/routes/index.routes.js
@@ -0,0 +1,8 @@
+import coloresRoutes from "./color.routes.js";
+import { Router } from "express";
+
+const router = Router()
+
+router.use("/colores", coloresRoutes)
+
+export default router
